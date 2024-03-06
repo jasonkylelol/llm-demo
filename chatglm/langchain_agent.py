15f6287a3b252657f36bd82975a5b20dee7b42f4
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2024-03-06 02:47:36 +0000

    feat: chatglm support

--- chatglm/langchain_agent.py
+++ chatglm/langchain_agent.py
@@ -4,6 +4,7 @@ from langchain.agents import AgentExecutor, create_structured_chat_agent, load_t
 from langchain_core.messages import AIMessage, HumanMessage
 from chatglm3 import ChatGLM3
 from tools.weather import Weather
+from tools.state_of_the_union import State
 
 
 model_path = "/root/huggingface/models/THUDM/chatglm3-6b/"
@@ -13,18 +14,27 @@ if __name__ == "__main__":
     llm = ChatGLM3()
     llm.load_model(model_path)
     prompt = hub.pull("hwchase17/structured-chat-agent")
-    print(prompt.messages)
+    # print(prompt.messages)
 
-    tools = [Weather()]
+    tools = [
+        # Weather(),
+        State(),
+        ]
     agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    # ans = agent_executor.invoke(
+    #     {
+    #         "input": "厦门比北京热吗?",
+    #         "chat_history": [
+    #             HumanMessage(content="北京温度多少度"),
+    #             AIMessage(content="北京现在12度"),
+    #         ],
+    #     }
+    # )
+
     ans = agent_executor.invoke(
         {
-            "input": "厦门比北京热吗?",
-            "chat_history": [
-                HumanMessage(content="北京温度多少度"),
-                AIMessage(content="北京现在12度"),
-            ],
+            "input": "In State Of The Union, who is Joshua Davis and what happened to him?"
         }
     )
     print(ans)
---
+++ chatglm/tools/state_of_the_union.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from typing import Type, Any, Optional
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_community.vectorstores.faiss import FAISS
+
+
+class StateInput(BaseModel):
+    content: str = Field(description="the content need to be searched by this tool")
+
+
+class State(BaseTool):
+    name = "state_of_the_union"
+    description = "Use for searching any content about State Of The union"
+    args_schema: Type[BaseModel] = StateInput
+    vector_db: Optional[FAISS] = None
+
+    def __init__(self):
+        super().__init__()
+
+        model_name = "/root/huggingface/models/BAAI/bge-large-en-v1.5"
+        model_kwargs = {"device": "cuda:2"}
+        encode_kwargs = {"normalize_embeddings": True}
+        embeddings_model = HuggingFaceBgeEmbeddings(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs
+        )
+
+        # loader = TextLoader("./state_of_the_union.txt")
+        loader = WebBaseLoader(
+            web_paths=("https://raw.githubusercontent.com/hwchase17/chat-your-data/master/state_of_the_union.txt",),
+        )
+        documents = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=400, add_start_index=False
+        )
+        docs = text_splitter.split_documents(documents)
+        self.vector_db = FAISS.from_documents(docs, embeddings_model)
+
+
+    def _run(self, content: str) -> dict[str, Any]:
+        docs = self.vector_db.similarity_search(content, k=1)
+        return {"result": docs[0].page_content}
