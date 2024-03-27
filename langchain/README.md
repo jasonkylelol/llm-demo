2a7269b13b523174b9d7a206b02759e3c115d72d
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2024-03-27 11:36:53 +0800

    feat: langchain

--- langchain/README.md
+++ langchain/README.md
@@ -1,6 +1,5 @@
 # langchain implementation
 ## serve for HuggingFaceH4/zephyr-7b-beta model, backed with huggingface text-generation-inference api
-- langchain_hf_tgi_demo.py contains chat model(Llama2Chat) and default chain(LLMChain) implementation
-- langchain_custom_demo.py contains custom chain with llm(HuggingFaceTextGenInference) implementation, and custom ChatPromptTemplate and CallBack
-- langchain_retriever.py contains RAG implementation, use embeddings model and vector store for context retrieval
-- langchain_agent.py use local model(mistralai/Mistral-7B-Instruct-v0.2) without function calling to implement llm chat agent
+- hf_tgi_demo.py contains chat model(Llama2Chat) and default chain(LLMChain) implementation
+- custom_chain.py contains custom chain with llm(HuggingFaceTextGenInference) implementation, and custom ChatPromptTemplate and CallBack
+- rag.py contains RAG implementation, use embeddings model and vector store for context retrieval
---
+++ langchain/semantic_routing.py
@@ -0,0 +1,130 @@
+from langchain.utils.math import cosine_similarity
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from custom.zephyr_7b_custom_prompt_template import (
+    CustomChatPromptTemplate,
+    CustomCallbkHandler,
+)
+from langchain.prompts import (
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    ChatMessagePromptTemplate,
+    AIMessagePromptTemplate,
+)
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import torch
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+
+
+physics_template = """You are a very smart physics professor. \
+You are great at answering questions about physics in a concise and easy to understand manner. \
+When you don't know the answer to a question you admit that you don't know.
+
+Here is a question:
+{query}"""
+
+math_template = """You are a very good mathematician. You are great at answering math questions. \
+You are so good because you are able to break down hard problems into their component parts, \
+answer the component parts, and then put them together to answer the broader question.
+
+Here is a question:
+{query}"""
+
+
+model_path = "/root/huggingface/models/HuggingFaceH4/zephyr-7b-beta/"
+embedding_model_path = "/root/huggingface/models/BAAI/bge-large-en-v1.5"
+
+device = "cuda:2"
+embedding_device = "cuda:1"
+
+max_new_tokens=1024
+top_k=50
+top_p=0.65
+temperature=0.95
+
+embeddings_model, prompt_embeddings = None, None
+prompt_templates = [physics_template, math_template]
+
+
+def init_embeddings():
+    global embeddings_model, prompt_embeddings
+
+    model_name = embedding_model_path
+    model_kwargs = {"device": embedding_device}
+    encode_kwargs = {"normalize_embeddings": True}
+    embeddings_model = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    
+    prompt_embeddings = embeddings_model.embed_documents(prompt_templates)
+
+
+def init_llm():
+    init_embeddings()
+
+    model = AutoModelForCausalLM.from_pretrained(model_path,
+        device_map=device, torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained(model_path,
+        device_map=device, use_fast=True)
+    pipe = pipeline(
+        task='text-generation',
+        model=model,
+        tokenizer=tokenizer,
+        clean_up_tokenization_spaces=True,
+        return_full_text=False,
+        max_new_tokens=max_new_tokens,
+        do_sample=True,
+        temperature=temperature,
+        num_beams=1,
+        top_p=top_p,
+        top_k=top_k,
+        repetition_penalty=1.1,
+        pad_token_id=2,
+    )
+    llm = HuggingFacePipeline(pipeline=pipe)
+    return llm
+
+
+def prompt_router(input):
+    print("--------------------------------------------------------------")
+    query_embedding = embeddings_model.embed_query(input["query"])
+    similarity = cosine_similarity([query_embedding], prompt_embeddings)[0]
+    most_similar = prompt_templates[similarity.argmax()]
+    print("Using MATH" if most_similar == math_template else "Using PHYSICS")
+
+    template_messages = [
+        HumanMessagePromptTemplate.from_template(most_similar),
+        AIMessagePromptTemplate.from_template(""),
+    ]
+    prompt = CustomChatPromptTemplate.from_messages(template_messages)
+
+    # return PromptTemplate.from_template(most_similar)
+    print(prompt.invoke(input).to_string())
+    return prompt
+
+def post_process(input):
+    print(input)
+    print("--------------------------------------------------------------")
+    return input
+
+if __name__ == '__main__':
+    llm = init_llm()
+
+    chain = (
+        {"query": RunnablePassthrough()}
+        | RunnableLambda(prompt_router)
+        | llm
+        | StrOutputParser()
+        | RunnableLambda(post_process)
+    )
+
+    chain.invoke("What's a black hole")
+
+    chain.invoke("What's Trigonometric functions?")
+
+    chain.invoke("How humans survived on Mars?")
