2291330bc22653d22cfeae3a28a202816aa7f8ee
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2024-09-03 18:15:23 +0800

    feat: serving

--- serving/openai_api_server_llama3.sh
+++ serving/openai_api_server_llama3.sh
@@ -2,8 +2,8 @@
 
 MODEL=llama3/llama-3-chinese-8b-instruct-v2
 MODEL_PATH=/root/huggingface/models/$MODEL
-PORT=8061
+PORT=8060
 
 #CUDA_VISIBLE_DEVICES=1
 vllm serve $MODEL_PATH --served-model-name $MODEL --port $PORT --trust-remote-code \
-    --max-model-len 8192 --gpu-memory-utilization 0.95
+    --max-model-len 8192 --gpu-memory-utilization 0.9 --enforce-eager
