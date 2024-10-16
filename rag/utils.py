870a5a27257beb9ebc095ead71aad0ed68132712
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2024-10-16 16:49:34 +0800

    refactor: remove rag

--- rag/utils.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def md5sum_str(input_string):
-    byte_string = input_string.encode()
-    md5_hash = hashlib.md5()
-    md5_hash.update(byte_string)
-    return md5_hash.hexdigest()
