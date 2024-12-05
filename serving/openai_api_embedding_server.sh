f1a1c9017db33949ab25ac372cc2197bd578e7dc
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2024-12-05 16:53:04 +0800

    feat: remove serving

--- serving/openai_api_embedding_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-EmbeddingModel=maidalun1020/bce-embedding-base_v1
-
-PORT=8060 MODEL=/root/huggingface/models/$EmbeddingModel python -m open.text.embeddings.server
--- serving/openai_api_llama3_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MODEL=llama3/llama-3-chinese-8b-instruct-v2
-MODEL_PATH=/root/huggingface/models/$MODEL
-PORT=8060
-
-#CUDA_VISIBLE_DEVICES=1
-vllm serve $MODEL_PATH --served-model-name $MODEL --port $PORT --trust-remote-code \
-    --max-model-len 8192 --gpu-memory-utilization 0.9 --enforce-eager
